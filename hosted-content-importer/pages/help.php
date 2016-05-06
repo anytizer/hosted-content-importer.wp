f34a5b2e7e27cf40d8a9c95ba52ea6e96f30155f
Bimal Poudel <gist@bimal.org.np>
2016-05-06 16:20:58 -0600

    Improved results pages for [third] tags.

--- hosted-content-importer/pages/help.php
+++ hosted-content-importer/pages/help.php
@@ -6,27 +6,30 @@ SELECT
 	p.ID,
 	p.post_type,
 	p.post_title, 
-	p.guid 
+	p.guid,
+	p.post_date
 FROM {$wpdb->posts} p
 WHERE
 	p.post_status = 'publish'
 	AND p.post_type IN ('post', 'page')
-	AND p.post_content LIKE '[third%'
-ORDER BY p.post_date DESC
+	AND p.post_content LIKE '%[third%'
+ORDER BY
+	p.post_title ASC,
+	p.post_date DESC
 ";
 $posts = $wpdb->get_results($pages_query, OBJECT);
 ?>
 
 <div class="wrap">
-<h2>Posts and Pages using [third] tag</h2>
+<h2>HCI [third] Tagged</h2>
 <?php if ($posts): ?>
-	<p>The below listed titles contain [third] tags.</p>
+	<p>These posts/pages contain [third] tags.</p>
 	<ol>
 	<?php
 	foreach ($posts as $post)
 	{
 		$permalink = get_permalink($post->ID);
-		echo "<li>{$post->post_type}: <a href='{$post->guid}'>{$post->post_title}</a> - <a href='{$permalink}'>View</a> - <a href='post.php?post={$post->ID}&action=edit'>Edit</a></li>";
+		echo "<li>{$post->post_date}, {$post->post_type}: <a href='{$post->guid}'>{$post->post_title}</a> - <a href='{$permalink}'>View</a> - <a href='post.php?post={$post->ID}&action=edit'>Edit</a></li>";
 	}
 	?>
 	</ol>
