d874e7fed4cd8442cbee9b19ecca893e385540b5
Bimal Poudel <gist@bimal.org.np>
2016-05-08 02:48:51 -0600

    scripts to build/remove unused files in production

---
+++ hosted-content-importer/build.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+REM Run this file from /trunk/
+
+DEL /F /Q caches\*.cache
+DEL /F /Q classes/parsedown/download.sh
+DEL /F /Q classes/parsedown/download.bat
+DEL /F /Q build.sh
+DEL /F /Q build.bat
--- hosted-content-importer/build.sh
+++ hosted-content-importer/build.sh
@@ -4,4 +4,5 @@
 
 rm -f caches/*.cache
 rm -f classes/parsedown/download.sh
+rm -f classes/parsedown/download.bat
 rm -f build.sh
