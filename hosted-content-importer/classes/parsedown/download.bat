66778bcf08ba12904d2a874f7c53ac1641bb7c73
Bimal Poudel <gist@bimal.org.np>
2016-05-11 19:28:35 -0600

    Central build.

--- hosted-content-importer/classes/parsedown/download.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-curl -s -o Parsedown.php -O https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php
--- hosted-content-importer/classes/parsedown/download.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -O https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php
