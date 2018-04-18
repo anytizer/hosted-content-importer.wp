2f364cfb84656b1bbb0862e1dc201389fce1d865
server <server@example.com>
2018-04-18 17:52:18 -0600

    build scripts moved away

--- hosted-content-importer/build.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-REM Run this file from \trunk\
-
-curl -s -o classes\parsedown\Parsedown.php -O https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php
-
-DEL /F /Q caches\*.cache
-DEL /F /Q classes\parsedown\readme.md
-DEL /F /Q build.sh
-DEL /F /Q build.bat
--- hosted-content-importer/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run this file from /trunk/
-
-curl -s -o classes/parsedown/Parsedown.php -O https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php
-
-rm -f caches/*.cache
-rm -f classes/parsedown/readme.md
-rm -f build.sh
