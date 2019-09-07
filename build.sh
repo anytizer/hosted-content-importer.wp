9782e94955b337a9b5dff899aeed0602ef13dc65
anytizer <anytizer@bimal.org.np>
2019-09-06 21:58:51 -0600

    Download Parsedown license

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Run this file from /trunk/
 
 curl -s -o hosted-content-importer/classes/parsedown/Parsedown.php -O https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php
+curl -s -o hosted-content-importer/classes/parsedown/LICENSE.txt -O https://raw.githubusercontent.com/erusev/parsedown/master/LICENSE.txt
 
 rm -f caches/*.cache
 rm -f classes/parsedown/readme.md
